e9ab89dcdfd8562a0fee229866f35de35f8b06f4
adityach0uhan <m4with6x@gmail.com>
2024-03-05 22:47:03 +0530

    Creating sockets

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 import express from "express";
+import { createServer } from 'http'
+import {Server} from "socket.io"
 const app = express();
 const port = 3000;
-
+const server= createServer(app)
 
 app.get('/',(req,res)=> {
     res.send("Server is live");
 })
 
-app.listen(port, () => {
+const io = new Server(server);
+
+
+server.listen(port, () => {
     console.log(`Server is runnnig on port ${port}`);
 })
