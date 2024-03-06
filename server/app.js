434ccdae5e97f22c86218b869dc4357033892772
adityach0uhan <m4with6x@gmail.com>
2024-03-06 15:58:15 +0530

    Some changes

--- server/app.js
+++ server/app.js
@@ -1,15 +1,31 @@
 import express from "express";
 import { createServer } from 'http'
-import {Server} from "socket.io"
+import { Server } from "socket.io"
 const app = express();
 const port = 3000;
-const server= createServer(app)
+import cors from "cors"
+const server = createServer(app)
 
-app.get('/',(req,res)=> {
-    res.send("Server is live");
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send("Home route")
 })
 
-const io = new Server(server);
+const io = new Server(server,{
+    cors: {
+        origin: "http://localhost:5173" ,
+        methods: ["GET", "POST"],
+        credentials: true, 
+    },
+});
+
+
+io.on("connection", (socket) => {
+    console.log("User connected:")
+    console.log(socket.id)
+
+})
+
 
 
 server.listen(port, () => {
