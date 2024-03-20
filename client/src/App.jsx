f0e7c325be5b9cd81160b7d69ecb63c97c0bf378
adityach0uhan <m4with6x@gmail.com>
2024-03-20 23:37:55 +0530

    design changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
 import React, { useState, useEffect, useMemo } from 'react'
+import io from 'socket.io-client'
 
 const App = () => {
-
-  
-
+  const socket = io.connect("http://localhost:5000/")
+ 
 
   return (
     <>
-     
+      <div className='w-screen h-screen flex-col flex justify-center items-center '>
+        <div className='font-bold text-xl'>Live Chat  </div>
+        <div className='w-96 h-96 bg-blue-200 flex flex-col justify-center items-center'>
+
+
+          <div className='text-lg' >Header</div>
+          <div>Messages</div>
+          <div>Footer</div>
+          
+
+        </div>
+
+     </div>
     </>
   )
 }
