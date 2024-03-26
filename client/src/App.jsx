67b74248c39ed1ff03e4571bb8f1539b552da80f
adityach0uhan <m4with6x@gmail.com>
2024-03-26 23:33:13 +0530

    conditional rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useMemo } from 'react'
 import io from 'socket.io-client'
 import Chat from './Chat'
 const App = () => {
+  const socket = io.connect("http://localhost:5000/")
 
   const [username, setUsername] = useState("")
   const [chat, setChat] = useState(false)
@@ -16,28 +17,48 @@ const App = () => {
   }
 
 
-  const socket = io.connect("http://localhost:5000/")
-
-
   return (
     <>
 
       <div className='flex justify-center items-center flex-col w-screen h-screen'>
         {
-          (!chat) ? 
+          (!chat) ? (
             <div className='w-70 flex justify-center items-center flex-col w-80 border-spacing-1 rounded-lg h-80 gap-3 bg-blue-500'>
 
-        <input type="text" className='p-2 m-1 text-lg border-none outline-none rounded' placeholder='Username....' value={username} onChange={(e) => setUsername(e.target.value)} />
+              <input
+                type="text"
+                className='p-2 m-1 text-lg border-none outline-none rounded'
+                placeholder='Username....'
+                value={username}
+                onChange={
+                  (e) => setUsername(e.target.value)
+                } />
 
-        <input type="text" className='p-2 m-1 text-lg border-none outline-none rounded' placeholder='Room Id.....' value={roomId} onChange={(e) => { setRoomId(e.target.value) }} />
+              <input
+                type="text"
+                className='p-2 m-1 text-lg border-none outline-none rounded'
+                placeholder='Room Id.....'
+                value={roomId}
+                onChange={
+                  (e) => setRoomId(e.target.value)
+                } />
 
-        <button className='p-1 bg-green-500 w-40 m-1 text-lg border-none outline-none rounded' onClick={joinRoom}>Join Room</button>
-      </div>
-      :<Chat socket={socket} roomId={roomId} username={username} />
-      }
-      
-    </div >
-      </>
+              <button
+                className='p-1 bg-green-500 w-40 m-1 text-lg border-none outline-none rounded'
+                onClick={joinRoom}>
+                Join Room
+              </button>
+            </div>
+          ) : (
+              <Chat
+                socket={socket}
+                roomId={roomId}
+                username={username} />
+          )
+        }
+
+      </div >
+    </>
   )
 }
 
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -4,6 +4,7 @@ import { BsCaretRightFill } from "react-icons/bs";
 const Chat = ({ socket, roomId, username }) => {
 
     const [currentMsg, setcurrentMsg] = useState("")
+    const [allMessages, setAllmessages] = useState([]);
 
     const sendMessage = async () => {
 
@@ -15,14 +16,15 @@ const Chat = ({ socket, roomId, username }) => {
                 author: username,
             };
             await socket.emit("send_message", Msgdata)
+            setcurrentMsg('')
         }
-
     }
 
     useEffect(() => {
 
         socket.on("receive_message", (data) => {
-            console.log("front end", data)
+            console.log("Front End ",data)
+            // setAllmessages((list)=>{[...list,data]})
         })
 
 
@@ -38,10 +40,26 @@ const Chat = ({ socket, roomId, username }) => {
                 {/* Chat box  */}
                 <div className='w-96 h-96 bg-blue-200 relative flex flex-col justify-center items-center'>
                     <div className='text-lg h-10 flex items-center justify-center  w-full bg-green-600 text-center top-0 absolute ' >{username}</div>
-                    <div>Messages</div>
+                    <div className='bg-blue-300 w-full h-5 '>
+                        {
+                            allMessages.map((item) => {
+                                return <p>{item.message}</p>
+                            })
+                        }
+                    </div>
                     <div className='footer absolute bottom-0 w-full flex justify-between text-center items-center h-12  overflow-hidden '>
-                        <input onChange={(e) => setcurrentMsg(e.target.value)} className='w-3/4  h-full px-3 outline-none border-none text-xl' type="text" />
-                        <button onClick={sendMessage} className=' w-1/4 h-full border-none outline-none  text-center  bg-green-500 '><div className='flex w-full justify-center items-center text-lg '><BsCaretRightFill /></div></button>
+                        <input
+                            onChange={(e) => setcurrentMsg(e.target.value)}
+                            className='w-3/4  h-full px-3 outline-none border-none text-xl'
+                            value={currentMsg}
+                            type="text" />
+                        <button
+                            onClick={sendMessage}
+                            className=' w-1/4 h-full border-none outline-none  text-center  bg-green-500 '>
+                            <div className='flex w-full justify-center items-center text-lg '>
+                                <BsCaretRightFill />
+                            </div>
+                        </button>
                     </div>
 
 
